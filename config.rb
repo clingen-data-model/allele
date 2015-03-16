e14fc2fc5d8112e8e9a798ea1a849e775e873204
Tristan Nelson <tristan@iseult.us>
2015-03-16 14:18:24 -0400

    Completing automated display of resource examples

--- config.rb
+++ config.rb
@@ -70,11 +70,14 @@ helpers do
       output
     end
   end
+  
+  def example_path(example_id)
+    "/allele/implementation/examples/#{example_id}.html"
+  end
 
   def examples_index
     output = data.examples.reduce("<ul>\n") do |acc, e|
-      acc + "<li>" + link_to(e['title'],
-                             "/allele/implementation/examples/#{e['id']}.html")
+      acc + "<li>" + link_to(e['title'], example_path(e['id']))
     end
     return output + "</ul>"
   end
@@ -86,6 +89,21 @@ helpers do
     else
       output
     end
+  end
+
+  def examples_table(resource_name)
+    examples = data.examples.select do |example|
+      example.resource == resource_name
+    end
+    header = '<table class="table"><thead><tr><th>id</th><th>name</th></tr></thead><tbody>'
+    output = examples.reduce(header) do |acc, example|
+      acc << '<tr>'
+      acc << "<td>#{example['id']}</td>"
+      acc << "<td>#{link_to example['title'], example_path(example['id']) }</td>"
+      acc << "<td>#{link_to 'xml', example['xml']}</td>"
+      acc << "<td>#{link_to 'json', example['json']}</td>"
+    end
+    output + "</tbody></table>"
   end
 
 end
