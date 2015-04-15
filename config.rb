478126774406caa166b7f30f406658541600a50b
Bradford Powell <bpow@drpowell.org>
2015-04-15 10:04:49 -0400

    re-enable middleman livereload

--- config.rb
+++ config.rb
@@ -42,9 +42,9 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 helpers do
