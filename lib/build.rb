24bc58dbcb4c8ef4838b2ed162b546eceddac7ef
Tristan Nelson <tristan@iseult.us>
2015-08-27 19:39:21 -0400

    WIP constructing build script

--- lib/build.rb
+++ lib/build.rb
@@ -1,6 +1,29 @@
 #!/env/ruby
 
 require 'git'
+require 'yaml'
+require 'fileutils'
+
+
+# load the list of versions and delete the item
+# corresponding to the master branch
+versions = YAML.load_file('data/versions.yml')
+versions.delete('development')
 
 main_repo = Git.open(Dir.pwd)
-puts main_repo.status
+
+# checkout each version in turn, build the documentation for that version,
+# then copy it into the versioned folder under /stage
+versions.each do |v|
+  main_repo.checkout(v)
+  # TODO check for and handle build errros
+  `bundle exec middleman build`
+  FileUitls.mv('build', "stage/#{v}")
+end
+
+# Finish by building current docs
+main_repo.checkout
+`bundle exec middleman build`
+FileUtils.mv('build', "stage/#{development}")
+
+
