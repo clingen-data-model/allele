729582a790e97f311259e4768f942f6cd7dacfee
Nafisa Khandaker <nkhandaker@partners.org>
2017-07-20 12:36:12 -0400

    changed config file

--- config.rb
+++ config.rb
@@ -243,6 +243,13 @@ end
 
 end
 
+# Enable Asset Hosts
+activate :asset_host
+    
+set :asset_host do |asset|
+      '//http://datamodel.clinicalgenome.org/clingen-static-resources/'.to_s
+end
+
 set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
@@ -258,7 +265,7 @@ configure :build do
   # activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   activate :relative_assets
